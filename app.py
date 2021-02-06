75d0d55851f492558173d9c28cf52b058ff95f56
AlexOff <alexov2004@gmail.com>
2021-02-07 02:57:57 +0300

    сделал методы для получения схемы класса и возврат всех данных со склада в формате JSON

--- app.py
+++ app.py
@@ -1,17 +1,49 @@
+import json
+
 from flask import Flask, render_template, request, redirect, url_for
 
+from talk_to_back import BackendTalker
 
 application = Flask(__name__)
+
+talker = BackendTalker(port=3000, host="192.168.0.109")
+
 
 @application.route('/')
 def hello_world():
     return "HighLevelAPI made by ПиФла"
+
 
 @application.route('/get_scheme')
 def ret_csheme():
     return redirect("http://192.168.0.109:3000/get_storage_scheme")
 
 
+@application.route('/get_list_of_items_main_json')
+def ret_all_storage():
+    data = talker.get_list_of_all()
+    _ = []
+    for i in data:
+        _.append({
+            "name": i.name,
+            "merged": "0" if i.merged else "1",
+            "group_of_merge": str(i.group_of_merge),
+            "merged_with": "" if not i.merged else str(i.merged_with),
+            "size_width": i.size_width,
+            "size_height": i.size_height,
+            "size_depth": i.size_depth,
+            "busy": "0" if i.busy else "1",
+            "contained_item": "" if not i.busy else {
+                "name": i.contained_item.name,
+                "height": i.contained_item.original_height,
+                "width": i.contained_item.original_width,
+                "depth": i.contained_item.original_depth,
+                "mass": i.contained_item.mass,
+                "uuid": i.contained_item.uuid
+            }
+        })
+
+    return json.dumps(_)
 
 
 if __name__ == '__main__':
